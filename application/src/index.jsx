3ab3383fc9be57772f96d52121aee5dc9105f7cd
Killian Elie <killianelie@gmail.com>
2023-10-02 15:04:08 +0200

    update repository

---
+++ application/src/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './main.css';
+import Home from './pages/home';
+import Login from './pages/sign-in' ;
+import User from './pages/user';
+import Error from './pages/error';
+
+import Header from './components/header';
+import Footer from './components/footer';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <Router>
+      <div className='router'>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path= "/sign-in" element={<Login />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      <Footer />
+      </div>
+    </Router>
+  </React.StrictMode>
+);
